ddecd0571601f3f6b058e2d006bf88b6567c4220
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-10-07 21:00:42 +0200

    exercise 2, incomplete part

---
+++ src/main/java/sk/tuke/kpi/oop/game/Computer.java
@@ -0,0 +1,22 @@
+package sk.tuke.kpi.oop.game;
+
+import sk.tuke.kpi.gamelib.framework.AbstractActor;
+
+
+class Computer extends AbstractActor
+{
+    /*
+    public int add () {
+        return 0;
+    }
+    public float add () {
+        return 0;
+    }
+    public int sub () {
+        return 0;
+    }
+    public float sub () {
+        return 0;
+    }*/
+
+}
--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -1,5 +1,6 @@
 package sk.tuke.kpi.oop.game;
 
+import org.jetbrains.annotations.NotNull;
 import sk.tuke.kpi.gamelib.framework.AbstractActor;
 import sk.tuke.kpi.gamelib.graphics.Animation;
 
@@ -8,16 +9,62 @@ class Reactor extends AbstractActor
 {
     private int temperature;
     private double damage;
+
     private Animation normalAnimation;
+    private Animation destroyAnimation;
+    private Animation overheatedAnimation;
+    private Animation offAnimation;
+
+    private boolean running;
 
     public Reactor () {
         this.temperature = 0;
-        this.damage = 0;
+        this.damage = 0.0D;
+        this.running = true;
 
         // create animation object
         this.normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        this.destroyAnimation = new Animation("sprites/reactor.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        this.overheatedAnimation = new Animation("sprites/reactor_hot.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        this.offAnimation = new Animation("sprites/reactor_broken.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+
+
         // set actor's animation to just created Animation object
         setAnimation(this.normalAnimation);
-
     }
+
+
+    public void updateAnimation() {
+        if (!this.running && this.damage != 100) {
+            super.setAnimation(offAnimation);
+        } else {
+            if (this.temperature < 4000) {
+                setAnimation(normalAnimation);
+            }
+            if (this.temperature > 4000 && this.temperature < 6000) {
+                setAnimation(overheatedAnimation);
+            }
+            if (this.damage == 100) {
+                running = false;
+                setAnimation(destroyAnimation);
+            }
+        }
+    }
+
+    public void increaseTemperature (int increment) {
+        this.temperature += increment;
+    }
+    public void decreaseTemperature (int decrement) {
+        this.temperature -= decrement;
+    }
+
+
+    public int getTemperature () {
+        return this.temperature;
+    }
+    public double getDamage () {
+        return this.damage;
+    }
+
+
 }
