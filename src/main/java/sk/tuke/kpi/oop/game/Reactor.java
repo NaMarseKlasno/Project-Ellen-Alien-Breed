b8bd3bf1b0d1626a72177dd93e1d10e40d6a7cd3
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-10-27 17:53:50 +0200

    test

--- src/main/java/sk/tuke/kpi/oop/game/Reactor.java
+++ src/main/java/sk/tuke/kpi/oop/game/Reactor.java
@@ -24,7 +24,7 @@ public class Reactor extends AbstractActor
 
         this.normalAnimation = new Animation("sprites/reactor_on.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
         this.destroyAnimation = new Animation("sprites/reactor.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
-        this.overheatedAnimation = new Animation("sprites/reactor_hot.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
+        this.overheatedAnimation = new Animation("sprites/reactor_hot.png", 80, 80, 0.05f, Animation.PlayMode.LOOP_PINGPONG);
         this.offAnimation = new Animation("sprites/reactor_broken.png", 80, 80, 0.1f, Animation.PlayMode.LOOP_PINGPONG);
 
         setAnimation(normalAnimation);
@@ -37,16 +37,19 @@ public class Reactor extends AbstractActor
     }
 
     public void increaseTemperature (int increment) {
+        if (increment < 1) return;
+
         this.temperature += increment;
-        if (this.temperature > 2000)
-            this.damage = ((this.temperature-2000)*100)/4000;
+        if (this.temperature > 2000) {
+            this.damage = ((this.temperature - 2000) * 100) / 4000;
+
+        }
         updateAnimation();
     }
     public void decreaseTemperature (int decrement) {
         this.temperature -= decrement;
         updateAnimation();
     }
-
 
     public void updateAnimation() {
         if (!this.running && this.damage != 100) {
@@ -65,7 +68,4 @@ public class Reactor extends AbstractActor
             }
         }
     }
-
-
-
 }
