e1071b9368f48a7ba9d042533e554e5b8e13df10
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-10-30 18:48:04 +0200

    fixed bug with Mjolnir

---
+++ src/main/java/sk/tuke/kpi/oop/game/actions/PerpetualReactorHeating.java
@@ -0,0 +1,2 @@
+package sk.tuke.kpi.oop.game.actions;public class PerpetualReactorHeating {
+}
--- src/main/java/sk/tuke/kpi/oop/game/tools/Mjolnir.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/Mjolnir.java
@@ -1,11 +1,26 @@
 package sk.tuke.kpi.oop.game.tools;
 
-import sk.tuke.kpi.oop.game.tools.Hammer;
+import sk.tuke.kpi.gamelib.graphics.Animation;
 
 
 public class Mjolnir extends Hammer {
 
+    private int number_of_uses;
+
     public Mjolnir() {
-        this.remainingUses = 4;
+        this.number_of_uses = 4;
+        setAnimation(new Animation("sprites/hammer.png"));
     }
+
+    @Override
+    public void use() {
+        if (this.number_of_uses == 0)
+            return;
+
+        --number_of_uses;
+
+        if (this.number_of_uses == 0)
+            getScene().removeActor(this);
+    }
+
 }
