51390de4daf07740520d6d33740a1bdb4da0227d
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-10-31 02:52:19 +0200

    added smart cooler

--- src/main/java/sk/tuke/kpi/oop/game/SmartCooler.java
+++ src/main/java/sk/tuke/kpi/oop/game/SmartCooler.java
@@ -1,2 +1,34 @@
-package sk.tuke.kpi.oop.game;public class SmartCooler {
+package sk.tuke.kpi.oop.game;
+
+import sk.tuke.kpi.gamelib.framework.actions.Loop;
+import sk.tuke.kpi.gamelib.actions.Invoke;
+
+import org.jetbrains.annotations.NotNull;
+import sk.tuke.kpi.gamelib.Scene;
+
+
+public class SmartCooler extends Cooler {
+
+    private Reactor REACTOR;
+
+    public SmartCooler(Reactor REACTOR) {
+        super(REACTOR);
+
+        this.REACTOR = REACTOR;
+    }
+
+
+    private void smartCooler() {
+        if (this.REACTOR == null) return;
+
+        if (this.REACTOR.getTemperature() >= 2500) turnOn();
+        if (this.REACTOR.getTemperature() <= 1500) turnOff();
+    }
+
+    @Override
+    public void addedToScene(@NotNull Scene scene) {
+        super.addedToScene(scene);
+        new Loop<>(new Invoke<>(this::smartCooler)).scheduleFor(this);
+    }
 }
+
