1421f264f96c6aea322a7b45d09ba9c3e507973d
Stanislav Voloshyn <stanislav.voloshyn@student.tuke.sk>
2021-11-05 20:09:29 +0100

    fix

--- src/main/java/sk/tuke/kpi/oop/game/tools/Wrench.java
+++ src/main/java/sk/tuke/kpi/oop/game/tools/Wrench.java
@@ -2,23 +2,27 @@ package sk.tuke.kpi.oop.game.tools;
 
 import sk.tuke.kpi.gamelib.graphics.Animation;
 import sk.tuke.kpi.oop.game.DefectiveLight;
+//import sk.tuke.kpi.oop.game.Repairable;
 
-public class Wrench extends BreakableTool<DefectiveLight>{
+public class Wrench extends BreakableTool<DefectiveLight> {
 
-    //private int health = 2;
+    //private Animation wrench;
 
-
-    public Wrench(){
+    public Wrench() {
         super(2);
-        Animation wrenchAnimation = new Animation("sprites/wrench.png");
-        //setRemainingUses(health);
-        setAnimation(wrenchAnimation);
+
+        //this.wrench = new Animation("sprites/wrench.png");
+        setAnimation(new Animation("sprites/wrench.png"));
     }
+//    public Hammer() {
+//        this(1);
+//    }
+
     @Override
-    public void useWith(DefectiveLight actor) {
-        if(actor != null && this.getRemainingUses() > 0 && !actor.isX() && actor.isPowered()) {
-            actor.repair();
-            super.useWith(actor);
-        }
+    public void useWith(DefectiveLight LIGHT){
+        if (LIGHT == null || super.getRemainingUses() < 1 || LIGHT.isPochynena()) return;
+        // repairable!=null && repairable.repair()
+        super.useWith(LIGHT);
+        LIGHT.repair();
     }
 }
